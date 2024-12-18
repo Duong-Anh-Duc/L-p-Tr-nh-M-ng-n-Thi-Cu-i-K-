37adf9634746ce3116dbb5f1dc8d319c4509ab40
Duong Anh Duc <101088372+Duong-Anh-Duc@users.noreply.github.com>
2024-12-18 16:42:44 +0700

    Update Client.java

--- RMI/RMI - Byte/1/Client.java
+++ RMI/RMI - Byte/1/Client.java
@@ -6,11 +6,9 @@ public class Client {
         Registry registry = LocateRegistry.getRegistry("203.162.10.109", 1099);
         ByteService ds = (ByteService) registry.lookup("RMIByteService");
         byte[] data = ds.requestData("B21DCCN239", "06W1XKWV");
-        byte[] send = new byte[data.length];
         for(int i = 0 ; i < data.length;i++){
             data[i] += data.length;
-            send[i] = data[i];
         }
-        ds.submitData("B21DCCN239", "06W1XKWV", send);
+        ds.submitData("B21DCCN239", "06W1XKWV", data);
     }
 }
