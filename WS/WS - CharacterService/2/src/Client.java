f5c70e6fb1f20948f71f7128c26ffdf3c60f9ea4
Duong Anh Duc <101088372+Duong-Anh-Duc@users.noreply.github.com>
2024-12-18 21:06:41 +0700

    Update Client.java

--- WS/WS - CharacterService/2/src/Client.java
+++ WS/WS - CharacterService/2/src/Client.java
@@ -8,42 +8,25 @@ import vn.medianews.CharacterService;
 import vn.medianews.CharacterService_Service;
 
 public class Client {
-    public static int dem(String x) {
-        String na = "ueoai";
-        int cnt = 0;
-        for (char i : x.toCharArray()) {
-            i = Character.toLowerCase(i);
-            if (i == 'u' || i == 'e' || i == 'o' || i == 'a' || i == 'i')
-                ++cnt;
-        }
-        return cnt;
-    }
+   
 
     public static void main(String[] args) {
         CharacterService_Service service = new CharacterService_Service();
         CharacterService characterService = service.getCharacterServicePort();
-        List<String> arr = characterService.requestStringArray("B21DCCN791", "DYeISPeq");
-        List<String> result = new ArrayList<>();
-        Collections.sort(arr);
-        int cnt[] = new int[1001];
-        cnt[0] = 1;
-        for (String x : arr) {
-            cnt[dem(x)] = 1;
-        }
-
-        for (int i = 0; i <= 1000; i++) {
-            if (cnt[i] != 0) {
-                String tmp = "";
-                for (String x : arr) {
-                    if (dem(x) == i) {
-                        tmp += x + ", ";
-                    }
-                }
-                tmp = tmp.substring(0, tmp.length() - 2);
-                result.add(tmp);
+        List<String> arr = characterService.requestStringArray("B21DCCN440", "LGPkNHFP");
+        int min_length = 99, max_length = -99;
+        String wordMin = "", wordMax = "";
+        for(String x : arr){
+            if(min_length > x.length()){
+                wordMin = x;
+                min_length = x.length();
+            }
+            if(max_length < x.length()){
+                wordMax = x;
+                max_length = x.length();
             }
         }
-        characterService.submitCharacterStringArray("B21DCCN791", "DYeISPeq", result);
+       characterService.submitCharacterString("B21DCCN440", "LGPkNHFP", wordMax + ";" + wordMin);
     }
 
 }
